b2fe58b285edc07ff6cdcefc429f0eafb4718793
steamingice <lucbroennimann2005@gmail.com>
2023-12-22 22:02:03 +0100

    commit

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: "ProductItem",
+  props: {
+    product: Object
+  }
+};
+</script>
+
+<template>
+  <div class="product-item">
+    <img :src="`/assets/${product.image}`" class="product-image" />
+    <p class="product-price">{{ product.price }}</p>
+    <i v-if="product.status ==1" class="bi bi-check-circle-fill"></i>
+    <i v-else class="bi bi-check-circle"></i>
+    <h3>{{ product.name }}</h3>
+    <p class="product-description">{{ product.description }}</p>
+    <i v-if="product.rating >= 1" class="bi bi-star-fill"></i>
+    <i v-else class="bi bi-star"></i>
+    <i v-if="product.rating >= 2" class="bi bi-star-fill"></i>
+    <i v-else class="bi bi-star"></i>
+    <i v-if="product.rating >= 3" class="bi bi-star-fill"></i>
+    <i v-else class="bi bi-star"></i>
+    <i v-if="product.rating >= 4" class="bi bi-star-fill"></i>
+    <i v-else class="bi bi-star"></i>
+    <i v-if="product.rating >= 5" class="bi bi-star-fill"></i>
+    <i v-else class="bi bi-star"></i>
+  </div>
+</template>
+
+
+
+
+<style>
+.product-item {
+  padding: 1rem;
+  margin: 1rem;
+  width: 300px;
+  text-align: center;
+}
+
+.product-image {
+  max-width: 100%;
+  height: auto;
+  margin-bottom: 1rem;
+}
+
+.product-price {
+  color: red;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-top: 0.5rem;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,31 @@
+<script>
+import ProductItem from "./ProductItem.vue";
+
+export default {
+  name: "ProductList",
+  components: {
+    ProductItem
+  },
+  props: {
+    products: Array
+  }
+};
+</script>
+
+
+<template>
+  <div class="product-list">
+    <ProductItem v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+
+
+<style>
+.product-list {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 20px;
+}
+</style>
